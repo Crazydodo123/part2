0f7192e30faeed28f4848e2b6a1768b514d63c39
Crazydodo123 <peizheg@gmail.com>
2023-06-26 13:15:09 -0400

    part 2-2

---
+++ courseinfo/src/components/Course.js
@@ -0,0 +1,39 @@
+const Header = (props) => {
+    return <h1>{props.course}</h1>
+  }
+  
+const Total = (props) => {
+  const total = props.parts.reduce((acc, el) => acc + el.exercises, 0)
+    
+  return <strong>total of {total} exercises</strong>
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  
+  return (
+    <div>
+      {parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+
+  return (
+    <div>
+      <Header key={course.id} course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
